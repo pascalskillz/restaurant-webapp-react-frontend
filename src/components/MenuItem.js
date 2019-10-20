cca26d3512b2f050b83b7e9a722bed983f30f27b
Christopher Watson <christopher_watson@icloud.com>
2019-10-20 16:19:14 -0400

    full menu and menu categories flex and grid

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "../styles/MenuItem.css";
 
-
-
 const MenuItem = props => (
-  <div className="main-item-div">
-    
+  <div className="menu-item-div">
+    <div className="menu-item-image">
+      <img src={props.img} alt={props.name}/>
+    </div>
+    <div className="menu-item-text">
+      {props.name}
+    </div>
   </div>
 );
   
