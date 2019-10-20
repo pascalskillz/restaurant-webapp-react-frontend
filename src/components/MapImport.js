bc8fb2a13fd1f1015da950f8b8cfa0eabb8c4403
Sridhar <sriadhy11@gmail.com>
2019-10-20 13:51:20 -0400

    my local changes

--- src/components/MapImport.js
+++ src/components/MapImport.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import map from './maps';
+import maps from './maps';
 
 class MapImport extends Component {
     render() {
         return  (
             <div>
-                <map
+                <maps
                     google={this.props.google}
                     center={{lat: 18.5204, lng: 73.8567 }}
                     height='300px'
