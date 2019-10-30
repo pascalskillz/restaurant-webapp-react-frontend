8a82a19e7e95d7fc73ade61cd2cbb23533dc5331
Christopher Watson <christopher_watson@icloud.com>
2019-10-29 20:09:55 -0400

    bug fixes

--- src/App.js
+++ src/App.js
@@ -8,17 +8,10 @@ import Reservation from './containers/Reservation';
 import Signup from './containers/Signup';
 import Admin from './containers/Admin';
 import Navbar from './components/Navbar';
-<<<<<<< HEAD
 import Footer from './components/Footer';
 import Contact from './containers/Contact';
-=======
->>>>>>> 164d12511ccd8c49dd2434266705b4e18b3326d3
 
 import './styles/App.css';
-
-// FOOTER IMPORT GOES HERE !!
-// import Footer from './components/Footer
-import Footer from './components/Footer';
 
 
 const App = () => (
@@ -33,11 +26,8 @@ const App = () => (
             <Route exact path="/menu" component={Menu} />
             <Route exact path="/reservation" component={Reservation} />
             <Route exact path="/signup" component={Signup} />
-<<<<<<< HEAD
             <Route exact path="/contact" component={Contact} />
-=======
             <Route exact path="/admin" component={Admin} />
->>>>>>> e48398b79ab3bede8231a2ff24d2c46facc28755
             <Route component={Main} />
           </Switch>
         </div>   
@@ -46,29 +36,4 @@ const App = () => (
     </Router>
   </MyProvider>
 );
-<<<<<<< HEAD
 export default App
-=======
-
-export default App;
-
-// export class MapContainer extends Component {
-//   render() {
-//     return (
-//       <Map
-//         google={this.props.google}
-//         zoom={14}
-//         style={mapStyles}
-//         initialCenter={{
-//          lat: -1.2884,
-//          lng: 36.8233
-//         }}
-//       />
-//     );
-//   }
-// }
-
-// export default GoogleApiWrapper({
-//   apiKey: 'AIzaSyA7IlTzWHoo8InZXKMsMluISUIlN2xgdxQE'
-// })(MapContainer);
->>>>>>> 164d12511ccd8c49dd2434266705b4e18b3326d3
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -10,7 +10,6 @@ class Main extends Component {
         {({ }) => (
           <div>
 
-<<<<<<< HEAD
             <div className="jumbotron">
               <div className="carousel-div">
                 <Carousel
@@ -30,25 +29,6 @@ class Main extends Component {
                   <img src="https://images.unsplash.com/photo-1460306855393-0410f61241c7" alt='Slideshow Image2' />
                   <img src="https://images.unsplash.com/photo-1458642849426-cfb724f15ef7" alt='Slideshow Image3' />
                 </Carousel>
-=======
-          <div className="jumbotron">
-          
-            <div className="jumbo-left">
-            <img className="jumbo-img" src={img}/*"https://images.unsplash.com/photo-1466637574441-749b8f19452f"*/ alt="Food Image"/>
-                          </div>
-            <div className="jumbo-right">
-              <div>
-
-              </div>
-              <div className="jumbo-text-large">
-                
-                Welcome to Tandoori Restaurant
-              </div>
-              
-              <div className="jumbo-button-div">
-                <a className="jumbo-button button" href="/menu">Menu</a>
-                <a className="jumbo-button button" href="/reservation">Reservation</a>
->>>>>>> 164d12511ccd8c49dd2434266705b4e18b3326d3
               </div>
 
               <div class="jumbo-grid">
