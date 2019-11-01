1253787282cb3515840a4ce812aaa0f1b841b44f
Guangweimeng <55167827+Guangweimeng@users.noreply.github.com>
2019-11-01 16:57:15 -0400

    fix footer console error

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -9,49 +9,28 @@ class Main extends Component {
       <MyConsumer>
         {({ }) => (
           <div>
-
-<<<<<<< HEAD
             <div className="jumbotron">
               <div className="carousel-div">
                 <Carousel
                   width="500px"
-                  autoplay='true'
+                  autoplay="true"
                   autoplayInterval='2000'
-                  wrapAround='true'
-                  pauseOnHover='true'
+                  wrapAround="true"
+                  pauseOnHover="true"
                   renderCenterLeftControls={({ previousSlide }) => (
-                    <div onClick={previousSlide}><i class="carousel-button fas fa-chevron-circle-left"></i></div>
+                    <div onClick={previousSlide}><i className="carousel-button fas fa-chevron-circle-left"></i></div>
                   )}
                   renderCenterRightControls={({ nextSlide }) => (
-                    <div onClick={nextSlide}><i class="carousel-button fas fa-chevron-circle-right"></i></div>
+                    <div onClick={nextSlide}><i className="carousel-button fas fa-chevron-circle-right"></i></div>
                   )}
                 >
                   <img src="https://images.unsplash.com/photo-1534422298391-e4f8c172dddb" alt='Slideshow Image1' />
                   <img src="https://images.unsplash.com/photo-1460306855393-0410f61241c7" alt='Slideshow Image2' />
                   <img src="https://images.unsplash.com/photo-1458642849426-cfb724f15ef7" alt='Slideshow Image3' />
                 </Carousel>
-=======
-          <div className="jumbotron">
-          
-            <div className="jumbo-left">
-            <img className="jumbo-img" src={img}/*"https://images.unsplash.com/photo-1466637574441-749b8f19452f"*/ alt="Food Image"/>
-                          </div>
-            <div className="jumbo-right">
-              <div>
-
-              </div>
-              <div className="jumbo-text-large">
-                
-                Welcome to Tandoori Restaurant
-              </div>
-              
-              <div className="jumbo-button-div">
-                <a className="jumbo-button button" href="/menu">Menu</a>
-                <a className="jumbo-button button" href="/reservation">Reservation</a>
->>>>>>> 164d12511ccd8c49dd2434266705b4e18b3326d3
               </div>
 
-              <div class="jumbo-grid">
+              <div className="jumbo-grid">
                 <div className="jumbo-left">
                   <img className="jumbo-img" src={img}/*"https://images.unsplash.com/photo-1466637574441-749b8f19452f"*/ alt="Food Image" />
                 </div>
