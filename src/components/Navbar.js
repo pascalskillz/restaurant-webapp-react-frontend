7160f01fecc553ac0d52eecdadd8be058761bdd0
Abdulmajeed <s1253323@monmouth.edu>
2019-11-02 15:45:20 +0000

    menu transition

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { MyConsumer } from '../Context';
@@ -37,37 +38,56 @@ class Navbar extends Component {
               </div>
             </div>
             <div className='dropdown-div'>
-              <NavLink className='navbar-brand' to='/'>
-                HOME
-              </NavLink>
-              <NavLink className='menu' to='/menu'>
-                Menu
-                <ul className='list'>
-                  <li>item1</li>
-                  <li>item2</li>
-                  <li>item3</li>
-                  <li>item</li>
-                </ul>
-              </NavLink>
-              <NavLink className='navbar-brand' to='/reservation'>
-                Reservation
-              </NavLink>
-              <NavLink className='navbar-brand' to='/login'>
-                Login
-              </NavLink>
-              <NavLink className='navbar-brand' to='/signup'>
-                SIGNUP
-              </NavLink>
+              <NavLink className='navbar-brand' to='/'>HOME</NavLink>
               <NavLink
-                className='navbar-brand'
+                className='drop-menu'
                 to='/menu'
                 onClick={() => this.setToggleBox()}>
-                test
-              </NavLink>
+                Menu
+              </NavLink> 
+    
+              <NavLink className='navbar-brand' to='/reservation'>Reservation</NavLink>
+              <NavLink className='navbar-brand' to='/login'>Login</NavLink>
+              <NavLink className='navbar-brand' to='/signup'>SIGNUP</NavLink>
+             
             </div>
             <div id="menu-box" style={this.state.boxToggle ? {display: 'initial'} : {display: 'none'} }>
-              fake box
+            <div className="column" >
+             
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Soups</a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Tandoor Breads </a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Tandoori Specialties</a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Chicken Specialties</a></div>
             </div>
+
+            <div className="column" >
+            
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Lamb Specialties </a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Seafood Specialties </a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Vegetarian Specialties</a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Rice Specialties</a></div>
+
+            </div>
+            <div className="column" >
+             
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Dosai </a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Uthapam</a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">North Indian Dinner/Thali</a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Accompaniments </a></div>
+
+  
+            </div>
+             <div className="column" >
+              
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Desserts </a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Beverages</a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Soups/</a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Soups/</a></div>
+
+             </div>
+             <div className="fu-me"><button className="full-menu"><a href="/menu"> View Full Menu </a> </button></div>
+            </div>
+          
           </nav>
         )}
       </MyConsumer>
