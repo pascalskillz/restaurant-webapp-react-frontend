a9d38239893000324be6cef7fc1b36bf4391e277
Abdulmajeed <s1253323@monmouth.edu>
2019-11-11 18:46:33 +0000

    Abdul

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,7 +39,12 @@ class Navbar extends Component {
             </div>
             <div className='dropdown-div'>
               <NavLink className='navbar-brand' to='/'>HOME</NavLink>
-              <NavLink className='drop-menu' to='/menu'>Menu</NavLink> 
+              <NavLink
+                className='drop-menu'
+                to='/menu'
+                onClick={() => this.setToggleBox()}>
+                Menu
+              </NavLink> 
     
               <NavLink className='navbar-brand' to='/reservation'>Reservation</NavLink>
               <NavLink className='navbar-brand' to='/login'>Login</NavLink>
@@ -47,38 +52,38 @@ class Navbar extends Component {
             </div>
 
             <div id="menu-box" style={this.state.boxToggle ? {display: 'initial'} : {display: 'none'} }>
-            <div className="column" >
+            <div className="box-item" >
              
-             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Soups</a></div>
-             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Tandoor Breads </a></div>
-             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Tandoori Specialties</a></div>
-             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Chicken Specialties</a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a className="menu-list" href="/login">Soups</a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a className="menu-list" href="/menu">Tandoor Breads </a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a className="menu-list" href="/menu">Tandoori Specialties</a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a className="menu-list" href="/menu">Chicken Specialties</a></div>
             </div>
 
-            <div className="column" >
+            <div className="box-item" >
             
-             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Lamb Specialties </a></div>
-             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Seafood Specialties </a></div>
-             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Vegetarian Specialties</a></div>
-             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Rice Specialties</a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a className="menu-list" href="/menu">Lamb Specialties </a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a className="menu-list" href="/menu">Seafood Specialties </a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a className="menu-list" href="/menu">Vegetarian Specialties</a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a className="menu-list" href="/menu">Rice Specialties</a></div>
 
             </div>
-            <div className="column" >
+            <div className="box-item" >
              
-             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Dosai </a></div>
-             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Uthapam</a></div>
-             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">North Indian Dinner/Thali</a></div>
-             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Accompaniments </a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a className="menu-list" href="/menu">Dosai </a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a className="menu-list" href="/menu">Uthapam</a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a className="menu-list" href="/menu">North Indian Dinner/Thali</a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a className="menu-list" href="/menu">Accompaniments </a></div>
 
   
             </div>
-             <div className="column" >
+             <div className="box-item" >
               
-             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Desserts </a></div>
-             <div><img className='img11' src={logo2} alt='LOGO!'/> <a href="/menu">Beverages</a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a className="menu-list" href="/menu">Desserts </a></div>
+             <div><img className='img11' src={logo2} alt='LOGO!'/> <a className="menu-list" href="/menu">Beverages</a></div>
              
              </div>
-             <div className="fu-me"><button className="full-menu"><a href="/menu"> View Full Menu </a> </button></div>
+             <div className="fu-me"><button className="full-menu"><a className="menu-list" href="/menu"> View Full Menu </a> </button></div>
             </div>
           
           </nav>
