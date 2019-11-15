4ef95a5a557fd6505b1fabfa9b1747533d95cfb2
Guangweimeng <55167827+Guangweimeng@users.noreply.github.com>
2019-11-15 16:59:13 -0500

    contact change to head

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -8,44 +8,49 @@ class Contact extends Component {
   render() {
     return (
       <MyConsumer>
-      {({  }) => (
-        <div id="contact">
-        <div className="contact-form bg-primary p-2">
-          <h2 className="m-heading">Contact Us</h2>
-          <div className="address">
-          Tandoor India Cuisuine <br/>
-            1610 NJ-35, <br/>Ocean Township, <br/>NJ 07712, 
-            <br/>Located in: Orchard Plaza<br/>United States<br/>
-            +1 732-531-1944 <br/>
-            Tandorr@gmail.com
+        {({ }) => (
+          <div id="">
+            <div class="head">
+              <h2 className="m-heading">Contact Us</h2>
+              <p>We welcome your suggestions. Please send us your questions, feedback and comments.</p>
+            </div>
+            <div id="contact">
+              <div className="contact-form bg-primary p-2">
+                <div className="address">
+                  Tandoor India Cuisuine <br />
+                  1610 NJ-35, <br />Ocean Township, <br />NJ 07712,
+            <br />Located in: Orchard Plaza<br />United States<br />
+                  +1 732-531-1944 <br />
+                  Tandorr@gmail.com
           </div>
-          <p>Please use the form below to contact us</p>
-          <form>
-            <div className="form-group">
-              <label for="name">Name</label>
-              <input type="text" name="name" id="name" placeholder="Enter Name" />
+                <p>Please use the form below to contact us</p>
+                <form>
+                  <div className="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name" placeholder="Enter Name" />
+                  </div>
+                  <div className="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" placeholder="Enter Email" />
+                  </div>
+                  <div className="form-group">
+                    <label for="phone">Phone Number</label>
+                    <input type="text" name="phone" id="phone" placeholder="Enter Phone Number" />
+                  </div>
+                  <div className="form-group">
+                    <label for="message">Message</label>
+                    <textarea name="message" id="message" placeholder="Enter Message"></textarea>
+                  </div>
+                  <input type="submit" value="Send" className="btn btn-dark" />
+                </form>
+              </div>
+              <div className="map">
+                <LMap />
+              </div>
             </div>
-            <div className="form-group">
-              <label for="email">Email</label>
-              <input type="email" name="email" id="email" placeholder="Enter Email" />
-            </div>
-            <div className="form-group">
-              <label for="phone">Phone Number</label>
-              <input type="text" name="phone" id="phone" placeholder="Enter Phone Number" />
-            </div>
-            <div className="form-group">
-              <label for="message">Message</label>
-              <textarea name="message" id="message" placeholder="Enter Message"></textarea>
-            </div>
-            <input type="submit" value="Send" className="btn btn-dark" />
-          </form>
-        </div>
-        <div className="map">
-          <LMap />
-        </div>
-      </div>
-      )}
-    </MyConsumer>
+          </div>
+        )}
+      </MyConsumer>
     );
   }
 }
