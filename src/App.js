0dee6e94d64af4cdc02e0bcff0ecceb9bf5003fc
Pascal Uchubilo <pascalskillz@gmail.com>
2019-11-15 12:47:54 -0500

    setup detail page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { MyProvider } from './Context'
-import Login from './containers/Login';
-import Main from './containers/Main';
-import Menu from './containers/Menu';
-import Reservation from './containers/Reservation';
-import Signup from './containers/Signup';
-import Admin from './containers/Admin';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Contact from './containers/Contact';
-import ControlPanel from './containers/ControlPanel';
-import AuthRoute from './components/AuthRoute';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { MyProvider } from "./Context";
+import Login from "./containers/Login";
+import Main from "./containers/Main";
+import Menu from "./containers/Menu";
+import Reservation from "./containers/Reservation";
+import Signup from "./containers/Signup";
+import Admin from "./containers/Admin";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Contact from "./containers/Contact";
+import ControlPanel from "./containers/ControlPanel";
+import AuthRoute from "./components/AuthRoute";
+import MenuItemDetail from "./containers/MenuItemDetail";
 
-import './styles/App.css';
+import "./styles/App.css";
 
 const App = () => (
   <MyProvider>
     <Router>
       <div className="app">
-        <Navbar /> 
+        <Navbar />
         <div className="content">
           <Switch>
             <Route exact path="/" component={Main} />
@@ -29,13 +30,14 @@ const App = () => (
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/contact" component={Contact} />
             <Route exact path="/admin" component={Admin} />
+            <Route path="/item/" component={MenuItemDetail} />
             <AuthRoute exact path="/cPanel" component={ControlPanel} />
             <Route component={Main} />
           </Switch>
-        </div>   
+        </div>
       </div>
-      <Footer/> 
+      <Footer />
     </Router>
   </MyProvider>
 );
-export default App
+export default App;
