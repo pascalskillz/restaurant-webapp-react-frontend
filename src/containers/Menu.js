2530f64a25aabe871a6f8f3ea1906407160fb332
Guangweimeng <55167827+Guangweimeng@users.noreply.github.com>
2019-11-21 18:39:56 -0500

    update

--- src/containers/Menu.js
+++ src/containers/Menu.js
@@ -5,7 +5,7 @@ import { default as Item } from '../components/MenuItem';
 import '../styles/Menu.css';
 import API from "../utils/API";
 import img from '../img/old-logo.jpg';
-
+import {Link} from 'react-router-dom';
 class Menu extends Component {
   state = {
     menuItems: [
@@ -150,9 +150,13 @@ class Menu extends Component {
     )
 
     const allItemsList = this.state.allItems.map((item, index) => (
+      
       <div key={index} className="all-grid-item">
+        <Link to={'/item/'+item.id}>
         <Item img={item.imageUrl} name={item.itemName} price={item.itemPrice} id={item.id}/>
+        </Link>
       </div>
+     
     ))
 
     return (
@@ -201,5 +205,6 @@ class Menu extends Component {
     );
   }
 }
+
 
 export default Menu;
