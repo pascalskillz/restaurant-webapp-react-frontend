7a41d10b51e31532cd9d515a0d3a8cc25cb0e9d8
goldentiger1127 <guangweimeng@u.boisestate.edu>
2020-03-02 15:46:55 -0500

    push new function

--- src/store/chartStorage.js
+++ src/store/chartStorage.js
@@ -11,16 +11,27 @@ const storageName = "chartLocal";
 
 export default {
     addItem(key, value) {
-      console.log(`key${key},value${value}`);
-      let itemList = JSON.parse(localStorage.getItem(storageName));
-      itemList = itemList==null?[]:itemList;
-      if (itemList == null || itemList[key] === undefined) {
-          itemList.push(value);
-      } else {
-        itemList[key].price += 1;
-      }
-      localStorage.setItem(storageName, JSON.stringify(itemList));
+        console.log(`key${key},value${value}`);
+        let itemList = JSON.parse(localStorage.getItem(storageName));
+        itemList = itemList==null?[]:itemList;
+        if (itemList == null || itemList[key] === undefined) {
+            itemList.push(value);
+        } else {
+          itemList[key].price += 1;
+        }
+        localStorage.setItem(storageName, JSON.stringify(itemList));
     },
+
+    updateItem() {
+
+    },
+    /**
+     * Remove some items in the localstorage
+     */
+    deleteItem(){
+
+    },
+
     clearChart(){
         localStorage.setItem(storageName,null);
     },
