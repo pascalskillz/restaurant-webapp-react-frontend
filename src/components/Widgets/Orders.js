106a090735f40f35529b594177b59a8589cf23c7
Pascal Uchubilo <pascalskillz@gmail.com>
2020-03-25 22:06:04 -0400

    implement orderDetail toggle

--- src/components/Widgets/Orders.js
+++ src/components/Widgets/Orders.js
@@ -4,7 +4,9 @@ import API from '../../utils/API';
 class Orders extends Component {
 
   state = {
-    orders: []
+    orders: [],
+    displayOrderDetails: false,
+    orderIDToView: -1
   };
 
   componentDidMount() {
@@ -24,6 +26,14 @@ class Orders extends Component {
     });
   };
 
+  viewOrderDetails = (id) => {
+    console.log(id)
+    this.setState({
+      displayOrderDetails: true,
+      orderIDToView: id
+    })
+  }
+
   render() {
     const allOrdersList = this.state.orders.map((order, index) => (
       <tr key={index}>
@@ -31,11 +41,18 @@ class Orders extends Component {
         <td>{order.orderDate}</td>
         <td>{order.customerName}</td>
         <td>{order.amount}</td>
-        <td><a className='btn btn-primary btn-order-view'> View </a></td>
+        <td><a onClick={() => this.viewOrderDetails(order.id)} className='btn btn-primary btn-order-view'>
+          View </a>
+        </td>
       </tr>
     ));
 
     return (
+
+      //check whether displayOrderDetails is true. If true, display orderdetails sections
+      //if false then display order list table
+      // we should have a back button in order details pages that turns the displayOrderDetails to false
+
       <div className='table-scroll edit-table'>
         <table className='table table-striped'>
           <thead className='thead-dark'>
