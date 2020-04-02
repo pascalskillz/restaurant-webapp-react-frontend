61dc2de8bd6c84b8e31e89825f98b7d91b819925
Pascal Uchubilo <pascalskillz@gmail.com>
2020-04-02 00:46:00 -0400

    setup orderdetails structure

--- src/components/Widgets/Orders.js
+++ src/components/Widgets/Orders.js
@@ -54,22 +54,69 @@ class Orders extends Component {
       // we should have a back button in order details pages that turns the displayOrderDetails to false
 
       //sort orders by date from backend
-
-      <div className='table-scroll edit-table'>
-        <table className='table table-striped'>
-          <thead className='thead-dark'>
-            <tr>
-              <th scope='col'>Order ID</th>
-              <th scope='col'>Order Date</th>
-              <th scope='col'>Customer Name</th>
-              <th scope='col'>Order Total</th>
-              <th scope='col'>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {allOrdersList}
-          </tbody>
-        </table>
+      <div className="order-widget">
+        <div className="order-details-container">
+          <div className="customer-order">
+            <div className="customer-info">
+              <p>Customer Name: <span> John Doe</span> </p>
+              <p>Customer Phone: <span> 7325773492</span></p>
+            </div>
+            <div className="order-info">
+              <p>Order date: <span> April 10, 2020</span></p>
+              <p>Grand Total: <span>$45</span></p>
+            </div>
+          </div>
+          <div className="details-table">
+            <div className="order-items">
+              <table className="table table-striped">
+                <thead className='thead-dark'>
+                  <tr>
+                    <th>ID</th>
+                    <th>Item Name</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                    <th>Total</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Chicken Cuisine</td>
+                    <td>5</td>
+                    <td>$ 12</td>
+                    <td>$ 60</td>
+                  </tr>
+                  <tr>
+                    <td>1</td>
+                    <td>Chicken Cuisine</td>
+                    <td>5</td>
+                    <td>$ 12</td>
+                    <td>$ 60</td>
+                  </tr>
+                  <tr className="grand-total">
+                    <td><span>Grand Total: </span>$120</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        {/* <div className='table-scroll order-table'>
+          <table className='table table-striped'>
+            <thead className='thead-dark'>
+              <tr>
+                <th scope='col'>Order ID</th>
+                <th scope='col'>Order Date</th>
+                <th scope='col'>Customer Name</th>
+                <th scope='col'>Order Total</th>
+                <th scope='col'>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {allOrdersList}
+            </tbody>
+          </table>
+        </div> */}
       </div>
     );
   }
