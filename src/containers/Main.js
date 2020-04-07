cbff4a44739e8a18a149931545de8d2ad624f98a
Abdulmajeed <s1253323@monmouth.edu>
2020-04-07 00:02:31 +0000

    Abdul

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -4,7 +4,11 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Carousel from 'nuka-carousel';
 import '../styles/Main.css';
-import img from '../img/jumbo-img.jpg';
+import img1 from '../img/slide1.jpg';
+import img2 from '../img/slide2.jpg';
+import img3 from '../img/slide3.jpg';
+import menu from '../img/menu1.jpg';
+
 class Main extends Component {
   render() {
     return (
@@ -16,11 +20,38 @@ class Main extends Component {
               <div className='carousel-div'>
                 <div className='carousel-text'>
                   <div className='jumbo-text-large'>
-                    Welcome to Tandoor Restauraunt
+                    Welcome to <br/>
+                    TANDOOR
+                    
                   </div>
                   <div className='jumbo-text-small'>
-                    We serve only the finest Indian cuisine
-                  </div>
+                  INDIAN RESTAURANT
+                    </div>
+                  <div className='main-content-button-div'>
+                  {/* <a className='main-content-button button' href='/menu'>
+                    Menu
+                  </a> */}
+                  <a className='main-content-button button' href='/reservation'>
+                  FIND A TABLE
+                  </a>
+                  <a className='main-content-button button' href='/menu'>
+                  VISIT MENU
+                  </a>
+                  
+                </div>
+                {/* <div className='main-content-div'>
+                <div className='main-content-button-div'>
+                  <a className='main-content-button button' href='/menu'>
+                    Check out the Menu
+                  </a>
+                  <a className='main-content-button button' href='/reservation'>
+                    Make a Reservation
+                  </a>
+                </div>
+                <div className='bottom'>{new Date().toString()}</div>}
+               
+              </div> */}
+                
                 </div>
                 <Carousel
                   // width='200px'
@@ -40,19 +71,23 @@ class Main extends Component {
                     </div>
                   )}>
                   <img
-                    src='https://images.unsplash.com/photo-1534422298391-e4f8c172dddb'
+                    src={img1}
+                    // 'https://images.unsplash.com/photo-1534422298391-e4f8c172dddb'
                     alt='Slideshow Image1'
                   />
                   <img
-                    src='https://images.unsplash.com/photo-1460306855393-0410f61241c7'
+                    src={img2}
+                    // 'https://images.unsplash.com/photo-1460306855393-0410f61241c7'
                     alt='Slideshow Image2'
                   />
                   <img
-                    src='https://images.unsplash.com/photo-1458642849426-cfb724f15ef7'
+                    src={img3}
+                    // 'https://images.unsplash.com/photo-1458642849426-cfb724f15ef7'
                     alt='Slideshow Image3'
                   />
                 </Carousel>
               </div>
+              
               <div className='utensils' id='section-divider'>
                 <div
                   className='utensil-container'
@@ -61,17 +96,25 @@ class Main extends Component {
                   <i className='fas fa-utensils'></i>
                 </div>
               </div>
-              <div className='main-content-div'>
-                <div className='main-content-button-div'>
-                  <a className='main-content-button button' href='/menu'>
-                    Check out the Menu
-                  </a>
-                  <a className='main-content-button button' href='/reservation'>
-                    Make a Reservation
-                  </a>
-                </div>
-                <div className='bottom'>{new Date().toString()}</div>
-              </div>
+              
+            <div className='main-content-div'>
+            <div class="row">
+            <div class="column">
+                Img1 
+            </div>
+            <div class="column">
+               img2
+            </div>
+            <div class="column">
+               img3
+            </div>
+            <div class="column">
+              TEXT
+             </div>
+            </div>
+
+              </div> 
+         
             </div>
             <Footer />
           </div>
